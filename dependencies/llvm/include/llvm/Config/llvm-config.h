918dbb1f67aecf4db88cbc307fa963e5f8b69302
0m0g1 <omogi.js@gmail.com>
2025-04-11 19:23:20 +0300

    update llvm to llvm 20

--- dependencies/llvm/include/llvm/Config/llvm-config.h
+++ dependencies/llvm/include/llvm/Config/llvm-config.h
@@ -109,7 +109,7 @@
 #define LLVM_HAS_SPARC_TARGET 1
 
 /* Define if the SPIRV target is built in */
-#define LLVM_HAS_SPIRV_TARGET 0
+#define LLVM_HAS_SPIRV_TARGET 1
 
 /* Define if the SystemZ target is built in */
 #define LLVM_HAS_SYSTEMZ_TARGET 1
@@ -142,16 +142,16 @@
 #define LLVM_USE_PERF 0
 
 /* Major version of the LLVM API */
-#define LLVM_VERSION_MAJOR 19
+#define LLVM_VERSION_MAJOR 20
 
 /* Minor version of the LLVM API */
 #define LLVM_VERSION_MINOR 1
 
 /* Patch version of the LLVM API */
-#define LLVM_VERSION_PATCH 7
+#define LLVM_VERSION_PATCH 2
 
 /* LLVM version string */
-#define LLVM_VERSION_STRING "19.1.7"
+#define LLVM_VERSION_STRING "20.1.2"
 
 /* Whether LLVM records statistics for use with GetStatistics(),
  * PrintStatistics() or PrintStatisticsJSON()
--- dependencies/llvm/include/llvm/Support/VCSRevision.h
+++ dependencies/llvm/include/llvm/Support/VCSRevision.h
@@ -1,2 +1,2 @@
-#define LLVM_REVISION "7f825cf74e5ea42e46b2b0a0020b4f781b81defd"
+#define LLVM_REVISION "b38df1abd23d9a5bcf70d260756bcd1b3a674b08"
 #define LLVM_REPOSITORY "https://github.com/msys2/MINGW-packages"
