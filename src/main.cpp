572c71b0cf43ff3b7665fec3141368c0f4de2d68
Davis Omogi <omogi.js@gmail.com>
2026-02-19 19:31:26 +0300

    major refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-#include <omniscript/Application.h>
+int main(int argc, char** argv) {
 
-int main(int argc, char* argv[]) noexcept {
-    Omniscript::Application app(argc, argv);
-    return app.run();
 }
