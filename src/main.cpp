da5b3128687cb93532328ccf860d7e7b007b5636
Davis Omogi <omogi.js@gmail.com>
2026-02-19 20:10:37 +0300

    restored

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
-int main(int argc, char** argv) {
+#include <omniscript/Application.h>
 
+int main(int argc, char* argv[]) noexcept {
+    Omniscript::Application app(argc, argv);
+    return app.run();
 }
